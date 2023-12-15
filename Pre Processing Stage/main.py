35f29b653a208b0298d97764c458f7e4f5b303ef
Ahmed Abdelatty <ahmedradalla2001@gmail.com>
2023-12-15 23:06:26 +0200

    pre processing stage with parameters

--- Pre Processing Stage/main.py
+++ Pre Processing Stage/main.py
@@ -9,29 +9,42 @@ from skimage.morphology import disk
 
 import commonfunctions
 
-def process(folder, rem_noise=False, gamma_corre=False, hist_eq=False):
+def process(
+        folder, 
+        rem_noise=False, 
+        gamma_corre=False, 
+        hist_eq=False, 
+        edge_det=False,
+        gamma=0.5,
+        median_kernel=disk(3),
+        edge_det_method="sobel"
+    ):
     # Read the images
     images = read_images_from_folder(folder)
     # Convert the images to grayscale (1 channel)
     images = [convert_to_grayscale(img) for img in images]
     # Show the original images
-    commonfunctions.show_images(images)
+    commonfunctions.show_images(images, titles=["Original Image" for i in range(len(images))])
     # Apply the median filter
-    images = [median(img, disk(3)) for img in images]
-    # Show the images after applying the median filter
-    commonfunctions.show_images(images)
-    # Apply gamma correction
-    images = [gamma_correction(img, 0.5) for img in images]
-    # Show the images after applying gamma correction
-    commonfunctions.show_images(images)
+    if rem_noise:
+        images = [median(img, median_kernel) for img in images]
+        # Show the images after applying the median filter
+        commonfunctions.show_images(images, titles=["Median Filter" for i in range(len(images))])
     # Apply histogram equalization
-    images = [histogram_equalization(img) for img in images]
-    # Show the images after applying histogram equalization
-    commonfunctions.show_images(images)
+    if hist_eq:
+        images = [histogram_equalization(img) for img in images]
+        # Show the images after applying histogram equalization
+        commonfunctions.show_images(images, titles=["Histogram Equalization" for i in range(len(images))])
+    # Apply gamma correction
+    if gamma_corre:
+        images = [gamma_correction(img, gamma) for img in images]
+        # Show the images after applying gamma correction
+        commonfunctions.show_images(images, titles=["Gamma Correction" for i in range(len(images))])
     # Apply edge detection
-    images = [detect_edges(img) for img in images]
-    # Show the images after applying edge detection
-    commonfunctions.show_images(images)
+    if edge_det:
+        images = [detect_edges(img, edge_det_method) for img in images]
+        # Show the images after applying edge detection
+        commonfunctions.show_images(images, titles=["Edge Detection" for i in range(len(images))])
 
     return images
 
@@ -55,5 +68,10 @@ def gamma_correction(img, gamma):
 def histogram_equalization(img):
     return sk.exposure.equalize_hist(img)
 
-def detect_edges(img):
-    return sk.feature.canny(img)
+def detect_edges(img, method="sobel"):
+    if method == "sobel":
+        return sk.filters.sobel(img)
+    elif method == "canny":
+        return sk.feature.canny(img)
+    else:
+        return sk.filters.sobel(img)
